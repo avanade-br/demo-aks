23d4016ff5ccef08505c8940e8a3b2a78ec12246
Samuel Barbosa <samuel.barbosa@avanade.com>
2020-01-17 11:43:35 -0300

    Update linkerd setup

--- 00-linkerd-setup.sh
+++ 00-linkerd-setup.sh
@@ -1,16 +1,28 @@
 #/bin/bash
 
-curl -sL https://run.linkerd.io/install | sh
-export PATH=$PATH:${HOME}/.linkerd2/bin
+# Instala o client do linkerd, se necessario
+if [ `find "${HOME}" -name linkerd | wc -l` -ne '1' ]; then
+   echo "curl -sL https://run.linkerd.io/install | sh"
+fi
 
+# Adiciona o linkerd no Path
+if [ `echo ${PATH} | grep -o linkerd | wc -l` -eq '0' ]; then
+   echo "export PATH=$PATH:${HOME}/.linkerd2/bin"
+fi
+
+# Pre-check
 linkerd check --pre
+
+# Instala o linkerd no cluster
 linkerd install | kubectl apply -f-
 
+# Pos check
+linkerd check
+
+# Instala o adapter do Prometheus (para ativar Autoscaling por metricas custom)
 helm install prometheus-adapter \
     --namespace kube-system \
     --set prometheus.url=http://linkerd-prometheus.linkerd.svc.cluster.local \
     --set prometheus.port=9090 \
     --set rules.default=true \
     stable/prometheus-adapter
-
-linkerd check
