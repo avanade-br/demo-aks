4b00a1da7686a9e17054ceb43f61f5b058222dd6
Samuel Barbosa <samuel.barbosa@avanade.com>
2020-01-17 11:45:59 -0300

    fix linkerd setup

--- 00-linkerd-setup.sh
+++ 00-linkerd-setup.sh
@@ -2,12 +2,12 @@
 
 # Instala o client do linkerd, se necessario
 if [ `find "${HOME}" -name linkerd | wc -l` -ne '1' ]; then
-   echo "curl -sL https://run.linkerd.io/install | sh"
+   curl -sL https://run.linkerd.io/install | sh
 fi
 
 # Adiciona o linkerd no Path
 if [ `echo ${PATH} | grep -o linkerd | wc -l` -eq '0' ]; then
-   echo "export PATH=$PATH:${HOME}/.linkerd2/bin"
+   export PATH=$PATH:${HOME}/.linkerd2/bin
 fi
 
 # Pre-check
